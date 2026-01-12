703f6d6e131edc12b15228267e60e07dfdf58de9
Sametsnrl5645 <sametsnrl5645@gmail.com>
2026-01-12 13:51:35 +0300

    Create __init__.py

---
+++ src/newai_prime/__init__.py
@@ -0,0 +1,8 @@
+# src/newai_prime/__init__.py içeriği:
+
+from .brain_core import NewaiBrainCore
+from .security_shield import SecurityShield
+from .model_set import ModelSettings
+# ... diğer 25 modülü buraya ekle ...
+
+print("⫸Ｎ⫷ Newai Prime Çekirdek Sistemi Yüklendi.")
